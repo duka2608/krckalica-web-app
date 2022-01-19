4c6a9f330e51680c0790a7ad5d44a26c6c0f000d
Dusko Stupar <dusko.stupar.97@gmail.com>
2022-01-19 22:50:19 +0100

    ubacen Gentelella admin template

---
+++ backend/resources/views/common/scripts.blade.php
@@ -0,0 +1,15 @@
+    <!-- jQuery -->
+    <script src="{{ asset('assets/gentelella/vendors/jquery/dist/jquery.min.js') }}"></script>
+    <!-- Bootstrap -->
+   <script src="{{ asset('assets/gentelella/vendors/bootstrap/dist/js/bootstrap.min.js') }}"></script>
+    <!-- FastClick -->
+    <script src="{{ asset('assets/gentelella/vendors/fastclick/lib/fastclick.js') }}"></script>
+    <!-- NProgress -->
+    <script src="{{ asset('assets/gentelella/vendors/nprogress/nprogress.js') }}"></script>
+    <!-- bootstrap-wysiwyg -->
+    <script src="{{ asset('assets/gentelella/vendors/bootstrap-wysiwyg/js/bootstrap-wysiwyg.min.js') }}"></script>
+    <script src="{{ asset('assets/gentelella/vendors/jquery.hotkeys/jquery.hotkeys.js') }}"></script>
+    <script src="{{ asset('assets/gentelella/vendors/google-code-prettify/src/prettify.js') }}"></script>
+
+    <!-- Custom Theme Scripts -->
+    <script src="{{ asset('assets/gentelella/build/js/custom.min.js') }}"></script>
