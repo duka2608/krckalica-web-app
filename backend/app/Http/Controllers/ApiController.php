fcdff78870bb1862d7fb6fb3dc6220d7b37ff1d7
Dusko Stupar <dusko.stupar.97@gmail.com>
2022-02-02 23:48:20 +0100

    Api endpoint-ovi za dohvatanje kategorija i kuhinja

--- backend/app/Http/Controllers/ApiController.php
+++ backend/app/Http/Controllers/ApiController.php
@@ -2,9 +2,19 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Category;
+use App\Models\Cuisine;
 use Illuminate\Http\Request;
 
 class ApiController extends Controller
 {
-    //
+    public function getCategories() {
+        $categories = Category::all();
+        return response()->json($categories, 200);
+    }
+
+    public function getCuisines() {
+        $cuisines = Cuisine::all();
+        return response()->json($cuisines, 200);
+    }
 }
--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ApiController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,6 +15,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+
+Route::get('get-categories', [ApiController::class, 'getCategories']);
+Route::get('get-cuisines', [ApiController::class, 'getCuisines']);
